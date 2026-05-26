ec31e8f6945374d4e44a32e11d84ba9f06c703fe
dnzgo <dnzgzc.98@gmail.com>
2026-05-27 01:04:35 +0200

    split check event

--- game_functions.py
+++ game_functions.py
@@ -7,20 +7,27 @@ def check_events(ship):
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right
-                    ship.moving_right = True
-                if event.key == pygame.K_LEFT:
-                    # Move the ship to the right
-                    ship.moving_left = True
+                check_keydown_events(event, ship)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    # stop moving the ship to the right
-                    ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    # stop moving the ship to the left
-                    ship.moving_left = False
+                check_keyup_events(event, ship)
 
+
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        # Move the ship to the right
+        ship.moving_right = True
+    if event.key == pygame.K_LEFT:
+        # Move the ship to the right
+        ship.moving_left = True
+
+
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        # stop moving the ship to the right
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        # stop moving the ship to the left
+        ship.moving_left = False
 
 def update_screen(settings, screen, ship):
      """Update images on the screen and flip to the new screen."""
